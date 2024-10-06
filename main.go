440e8721a10f9c9a0700517b4af1203ef8a443d5
lucaaaaum <lucasrenato.fernandesdasilva@gmail.com>
2024-10-06 12:58:13 -0300

    Criação da struct "processador"

--- main.go
+++ main.go
@@ -17,9 +17,23 @@ const (
 	Inválida
 )
 
-type Instrução struct {
+type instrução struct {
 	linhaDeOrigem string
 	tipo          TipoDeInstrução
 	parâmetros    []string
 	valores       []int
 }
+
+type processador struct {
+	clock                                    int
+	pc                                       int
+	registradores                            [32]int
+	labelsRegistradores                      map[string]int
+	memória                                  []int
+	labelsMemória                            map[string]int
+	instruções                               []string
+	fetch                                    string
+	decode, execute, memoryAccess, writeBack instrução
+}
+
+
