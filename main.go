483461b2542a0f4f8d9b3bddf88d67088dea097a
lucaaaaum <lucasrenato.fernandesdasilva@gmail.com>
2024-10-15 20:26:49 -0300

    Refatoração no main.go para permitir o argumento de predição

--- main.go
+++ main.go
@@ -2,18 +2,36 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"os"
+	"strconv"
 	"strings"
 
 	proc "github.com/lucaaaaum/mips/pkgs/processador"
 )
 
 func main() {
+	quantidadeDeArgumentos := len(os.Args)
+
+	if quantidadeDeArgumentos < 2 {
+		panic(errors.New("Deve ser fornecido um arquivo de instruções."))
+	}
+
+	if quantidadeDeArgumentos > 3 {
+		panic(errors.New("Quantidade de argumentos inesperada."))
+	}
+
 	linhasDeInstrução, err := obterLinhasDeInstruçãoDoArquivo()
 	if err != nil {
 		panic(err)
 	}
-	p := proc.New(linhasDeInstrução)
+
+	utilizarPredição, err := utilizarPredição()
+	if err != nil {
+		panic(err)
+	}
+
+	p := proc.New(linhasDeInstrução, utilizarPredição)
 	err = p.Processar()
 	if err != nil {
 		panic(err)
@@ -21,21 +39,13 @@ func main() {
 }
 
 func obterLinhasDeInstruçãoDoArquivo() ([]string, error) {
-	quantidadeDeArgumentos := len(os.Args)
-
-	if quantidadeDeArgumentos < 2 {
-		return nil, errors.New("Deve ser fornecido um arquivo de instruções.")
-	}
-
-	if quantidadeDeArgumentos > 2 {
-		return nil, errors.New("Somente um arquivo pode ser fornecido por vez.")
-	}
-
 	conteúdoDoArquivo, err := os.ReadFile(os.Args[1])
 	if err != nil {
 		return nil, err
 	}
+
 	linhasDeInstrução := strings.Split(string(conteúdoDoArquivo), "\n")
+
 	linhasDeInstruçãoFormatadas := make([]string, 0)
 	for i := 0; i < len(linhasDeInstrução); i++ {
 		linhasDeInstrução[i] = strings.TrimSpace(linhasDeInstrução[i])
@@ -44,5 +54,25 @@ func obterLinhasDeInstruçãoDoArquivo() ([]string, error) {
 		}
 		linhasDeInstruçãoFormatadas = append(linhasDeInstruçãoFormatadas, linhasDeInstrução[i])
 	}
+
 	return linhasDeInstruçãoFormatadas, nil
+}
+
+func utilizarPredição() (bool, error) {
+	if len(os.Args) < 3 {
+		return false, nil
+	}
+
+	parâmetroUtilizarPredição := os.Args[2]
+
+	númeroUtilizarPredição, err := strconv.Atoi(parâmetroUtilizarPredição)
+	if err != nil {
+		return false, err
+	}
+
+	if númeroUtilizarPredição != 1 && númeroUtilizarPredição != 0 {
+		return false, errors.New("O valor [" + fmt.Sprint(númeroUtilizarPredição) + "] é inválido. Por favor, passar 0 para false e 1 para true.")
+	}
+
+	return númeroUtilizarPredição == 1, nil
 }
